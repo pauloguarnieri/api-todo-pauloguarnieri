d0371fb514073c01f1aac0d54d60973fd474ba0b
kaiorr <kaio.rocharibeiro@gmail.com>
2023-03-15 14:02:48 -0300

    up

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+// CREATE
+router.post("/", async (req, res) => {});
+
+// READ ALL
+router.get("/", async (req, res) => {});
+
+// READ ONE
+router.get("/:id", async (req, res) => {});
+
+// UPDATE
+router.patch("/:id", async (req, res) => {});
+
+// DELETE
+router.delete("/:id", async (req, res) => {});
