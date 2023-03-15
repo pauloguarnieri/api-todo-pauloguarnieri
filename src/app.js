bd9211bf28914b1b3ca0b0344f2f03d563911fd0
Paulo <paulo.r.guarnieri@hotmail.com>
2023-03-15 17:18:24 -0300

    fix cors problem

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors");
+
+
+const database = [];
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
 
 // CREATE
-router.post("/", async (req, res) => {});
+app.post("/", async (req, res) => {
+    database.push(req.body)
+    return res.status(201).json(req.body);
+});
 
 // READ ALL
-router.get("/", async (req, res) => {});
+app.get("/", async (req, res) => {
+    return res.json(database);
+});
 
 // READ ONE
-router.get("/:id", async (req, res) => {});
+app.get("/:id", async (req, res) => { });
 
 // UPDATE
-router.patch("/:id", async (req, res) => {});
+router.patch("/:id", async (req, res) => { });
 
 // DELETE
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => { });
+
+app.listen(3000, () => {
+    console.log("Server running on port:3000")
+});
